fd5d4433b378a9ae35a7029126be6ec89353a1e6
Raghu vamsi <raghuvamsiponnamanda@gmail.com>
2026-03-29 13:41:22 +0530

    Added report for API module

--- api-module/src/test/java/com/precision/api/hooks/ApiHooks.java
+++ api-module/src/test/java/com/precision/api/hooks/ApiHooks.java
@@ -1,8 +1,11 @@
 package com.precision.api.hooks;
+
+import com.precision.common.logger.LogManager;
+import com.precision.common.reports.ExtentReportManager;
+import com.precision.common.reports.ExtentTestManager;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
-import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class ApiHooks {
@@ -10,16 +13,34 @@ public class ApiHooks {
     private static final Logger log = LogManager.getLogger(ApiHooks.class);
 
     @Before
-    public void beforeScenario(Scenario scenario) {
-        log.info("Starting scenario: " + scenario.getName());
+    public void setUp(Scenario scenario) {
+
+        ExtentTestManager.createTest(scenario.getName());
+
+        log.info("Starting Scenario: {}", scenario.getName());
+
+        ExtentTestManager.getTest()
+                .info("Starting Scenario: " + scenario.getName());
     }
 
     @After
-    public void afterScenario(Scenario scenario) {
+    public void tearDown(Scenario scenario) {
+
         if (scenario.isFailed()) {
-            log.error("Scenario FAILED: " + scenario.getName());
+            log.error("Scenario FAILED: {}", scenario.getName());
+
+            ExtentTestManager.getTest()
+                    .fail("Scenario Failed: " + scenario.getName());
+
         } else {
-            log.info("Scenario PASSED: " + scenario.getName());
+            log.info("Scenario PASSED: {}", scenario.getName());
+
+            ExtentTestManager.getTest()
+                    .pass("Scenario Passed: " + scenario.getName());
         }
+
+        ExtentTestManager.removeTest();
+        ExtentReportManager.flushReports();
+        log.info("Report updated");
     }
 }
